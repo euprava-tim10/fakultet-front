6fb2fed9b16979a9b4e78358b99a3e917669758c
borisavz <borisavzivanovic@gmail.com>
2023-04-21 19:40:28 +0200

    Prikaz rezultata konkursa studenu, upis na smer

--- src/app/pages/rezultati-konkurs/rezultati-konkurs.component.ts
+++ src/app/pages/rezultati-konkurs/rezultati-konkurs.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthManagerService } from 'src/app/auth/auth-manager.service';
+import { PrijavaKonkurs } from 'src/app/model/PrijavaKonkurs';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-rezultati-konkurs',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class RezultatiKonkursComponent {
 
+  principal: any;
+
+  prijave: PrijavaKonkurs[] = [];
+
+  constructor(
+    private studentService: StudentService,
+    private authManagerService: AuthManagerService
+  ) {}
+
+  ngOnInit() {
+    this.principal = this.authManagerService.getPrincipal();
+
+    this.studentService.getStudentovePrijaveKonkurs(this.principal.id).subscribe(p => this.prijave = p);
+  }
+
+  onPrihvatiPrijavu(prijava: PrijavaKonkurs) {
+    this.studentService.prihvatiPrijavu(this.principal.id, prijava.id).subscribe(x => {
+      prijava.statusPrijave = 'PRIHVACENA';
+    });
+  }
+
+  onOdbijPrijavu(prijava: PrijavaKonkurs) {
+    this.studentService.odbijPrijavu(this.principal.id, prijava.id).subscribe(x => {
+      prijava.statusPrijave = 'ODBIJENA';
+    });
+  }
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Student } from '../model/Student';
 import { ZavrsiStudije } from '../model/ZavrsiStudije';
+import { PrijavaKonkurs } from '../model/PrijavaKonkurs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +20,20 @@ export class StudentService {
   getStudent(studentId: number) {
     return this.httpClient.get<Student>(`${this.server}/api/studenti/${studentId}`, this.options);
   }
-
+  
   zavrsiStudije(studentId: number, statusStudijaId: number, zavrsiStudije: ZavrsiStudije) {
     return this.httpClient.put(`${this.server}/api/studenti/${studentId}/statusiStudija/${statusStudijaId}/zavrsi`, JSON.stringify(zavrsiStudije), this.options);
+  }
+
+  getStudentovePrijaveKonkurs(studentId: number) {
+    return this.httpClient.get<PrijavaKonkurs[]>(`${this.server}/api/studenti/${studentId}/prijave`, this.options);
+  }
+
+  prihvatiPrijavu(studentId: number, prijavaId: number) {
+    return this.httpClient.put(`${this.server}/api/studenti/${studentId}/prijave/${prijavaId}/prihvati`, '', this.options);
+  }
+
+  odbijPrijavu(studentId: number, prijavaId: number) {
+    return this.httpClient.put(`${this.server}/api/studenti/${studentId}/prijave/${prijavaId}/odbij`, '', this.options);
   }
 }
